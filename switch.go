6cb6d14917735ce41e0f343ea2fe816795daf089
obetron <erenbasaran@gmail.com>
2022-08-01 20:48:35 +0300

    switch examples completed

--- switch.go
+++ switch.go
@@ -25,4 +25,27 @@ func main() {
 		fmt.Println("It is a weekaday")
 	}
 
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I am a bool")
+		case int:
+			fmt.Println("I am a int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("Eren")
+
 }
