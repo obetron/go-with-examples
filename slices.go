77e1d23316bb9445de5801672f69595a2804772e
obetron <erenbasaran@gmail.com>
2022-08-02 22:36:55 +0300

    slices examples added

--- slices.go
+++ slices.go
@@ -28,4 +28,30 @@ func main() {
 
 	fmt.Println(intSlices)
 	//output: [2 3 234 0 434]
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy:", c)
+
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d:", twoD)
 }
