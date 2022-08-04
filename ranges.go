c7f04797dea9944033c8b74d68bfc336d25f67a0
ebasaran <ebasaran@havelsan.com.tr>
2022-08-04 10:08:59 +0300

    ranges example updated

--- ranges.go
+++ ranges.go
@@ -11,4 +11,27 @@ func main() {
 	}
 
 	fmt.Println("sum:", sum)
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	for t := range kvs {
+		fmt.Println("key:", t)
+	}
+
+	//range on strings iterates over Unicode code points.
+	//The first value is the starting byte index of the rune and the second the rune itself.
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+
 }
