bd860a719c90b8e505f186de09d50baea2857de3
obetron <erenbasaran@gmail.com>
2022-09-12 21:28:55 +0300

    new function examples added

--- 12-functions/functions.go
+++ 12-functions/functions.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func greeting() {
+	fmt.Println("Hello World")
+}
+
+func greetingWithName(name string) string {
+	greetingStr := "Hello, " + name
+	return greetingStr
+}
+
 func plus(a int, b int) int {
 	return a + b
 }
@@ -11,9 +20,21 @@ func plusPlus(a, b, c int) int {
 }
 
 func main() {
+
+	greeting()
+	greeting := greetingWithName("Test")
+	fmt.Println(greeting)
+
 	sum := plus(2, 5)
 	fmt.Println("2 + 5 = ", sum)
 
 	sumOfThree := plusPlus(2, 3, 5)
 	fmt.Println("2 + 3 + 5 =", sumOfThree)
+
+	x := 5
+	y := 10
+	z := 15
+
+	total := plusPlus(x, y, z)
+	fmt.Printf("%d + %d + %d = %d\n", x, y, z, total)
 }
