1ade5b1e43ab0853313a107cf6a17b796c7aa571
obetron <erenbasaran@gmail.com>
2022-09-14 23:40:40 +0300

    pass by value and pass by reference exmaples added

---
+++ 26-referenceby/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("PASS BY VALUE EXAMPLE")
+	passByValue()
+
+	fmt.Println("\nPASS BY REFERENCE EXAMPLE")
+	passByReference()
+}
+
+func passByValue() {
+	x := 5
+	fmt.Println("Before doubleInt method x is", x)
+	doubleInt(x)
+	fmt.Println("After doubleInt method x is", x)
+}
+
+func doubleInt(x int) {
+	x *= 2
+	fmt.Println("x double:", x)
+}
+
+func passByReference() {
+	mySlice := []int{1, 2, 3}
+	fmt.Println("Before doubleSlice method mySlice is", mySlice)
+	doubleSlice(mySlice)
+	fmt.Println("After doubleSlice method mySlice is", mySlice)
+}
+
+func doubleSlice(mySlice []int) {
+	for i := 0; i < len(mySlice); i++ {
+		mySlice[i] *= 2
+	}
+	fmt.Println("mySlice double:", mySlice)
+}
